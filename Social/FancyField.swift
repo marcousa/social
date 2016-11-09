6714eb2d8772023aa111c7b17117ab5fdd85fb2a
marcousa <marcodefilippo@gmail.com>
2016-11-09 15:23:12 -0500

    modified UI Styling

--- Social/FancyField.swift
+++ Social/FancyField.swift
@@ -12,9 +12,9 @@ class FancyField: UITextField {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.2).cgColor
         layer.borderWidth = 1.0
+        layer.cornerRadius = 5.0
     }
     
     //Impacts placeholder text only
@@ -22,6 +22,7 @@ class FancyField: UITextField {
         return bounds.insetBy(dx: 10, dy: 5)
     }
     
+    //Impacts user entered text
     override func editingRect(forBounds bounds: CGRect) -> CGRect {
         return bounds.insetBy(dx: 10, dy: 5)
     }
--- Social/FancyView.swift
+++ Social/FancyView.swift
@@ -13,11 +13,6 @@ class FancyView: UIView, lightShadow {
     override func awakeFromNib() {
         super.awakeFromNib()
         
-//        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
-//        layer.shadowOpacity = 0.8
-//        layer.shadowRadius = 5.0
-//        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        
         setShadow(toItem: layer)
         
     }
