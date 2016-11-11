492e71a199359ee306ccb2e7b741cbd48268b50a
marcousa <marcodefilippo@gmail.com>
2016-11-11 09:50:25 -0500

    Added email authentication with Firebase

--- Social/SignInVC.swift
+++ Social/SignInVC.swift
@@ -13,6 +13,9 @@ import Firebase
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var emailField: FancyField!
+    @IBOutlet weak var passwordField: FancyField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -56,6 +59,36 @@ class SignInVC: UIViewController {
         })
         
     }
+    
+    @IBAction func signInTapped(_ sender: AnyObject) {
+        
+        if let email = emailField.text, let pwd = passwordField.text {
+            
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                
+                if error == nil {
+                    print("APP: Authenticated successfully via email with Firebase")
+                } else {
+                    
+                    FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        
+                        if error != nil {
+                            print("APP: error creating new user in Firebase using email")
+                        } else {
+                            print("APP: User created in Firebase with email")
+                        }
+                        
+                    })
+                }
+                
+            })
+            
+        } else {
+            print("APP: No text entered in the email or password field")
+        }
+        
+    }
+    
 
 }
 
