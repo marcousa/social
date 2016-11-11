2dc4e1868b1413d316709a35026c90f5cde304c5
marcousa <marcodefilippo@gmail.com>
2016-11-11 15:16:14 -0500

    completed UI

---
+++ Social/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  Social
+//
+//  Created by Marco De Filippo on 11/11/16.
+//  Copyright © 2016 2AM App Labs. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: CircleImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+}
